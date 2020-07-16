1a1d3679f60eb37317cdeff3eb2ca964d3ecb23d
Brian Sunday <brians931@gmail.com>
2020-07-16 17:48:30 +0300

    add read endpoints

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,32 +6,3 @@ mongoose.connect(`mongodb://127.0.0.1:27017/task-manager-api`, {
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-
-
-
-// const Task = mongoose.model("Task", {
-//   description: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//   },
-
-//   completed: {
-//     type: Boolean,
-//     default: false,
-//   },
-// });
-
-// const tsk = new Task({
-//   description: "one",
-//   completed: false,
-// });
-
-// tsk
-//   .save()
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
--- src/index.js
+++ src/index.js
@@ -1,20 +1,78 @@
 const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 app.use(express.json());
 
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
 
-app.post("/users",(req,res)=>{
-  const user = new User(req.body)
+  user
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((error) => res.status(400).json(error));
+});
 
-  user.save().then(result=>{
-    res.status(201).json(result)
-  }).catch(error => res.status(400).json(error))
-})
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((result) => {
+      if (!result) {
+        return res.status(400).json({ message: "users not found" });
+      }
+      res.status(201).json(result);
+    })
+    .catch((error) => res.status(500).json(error));
+});
 
+app.get("/users/:id", (req, res) => {
+  const _id = req.param.id;
+  User.findById(_id)
+    .then((result) => {
+      if (!result) {
+        return res.status(400).json({ message: "users not found" });
+      }
+      res.status(201).json(result);
+    })
+    .catch((error) => res.status(500).json(error));
+});
 
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((error) => res.status(400).json(error));
+});
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((result) => {
+      if (!result) {
+        return res.status(400).json({ message: "tasks not found" });
+      }
+      res.status(200).json(result);
+    })
+    .catch((error) => res.status(500).json(error));
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.param.id;
+  Task.findById(_id)
+    .then((result) => {
+      if (!result) {
+        return res.status(400).json({ message: "task not found" });
+      }
+      res.status(200).json(result);
+    })
+    .catch((error) => res.status(500).json(error));
+});
 
 app.listen(5000, () => {
   console.log("server started at port 5000");
